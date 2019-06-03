718bf8faff1098e78fca43f6a8e4581a773c0e55
ak-felix <47936818+ak-felix@users.noreply.github.com>
2019-06-03 15:47:18 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+  $scope.title = "felix._.ak";
+  $scope.bioSummary = "To contact..Follow me in the following platforms";
+  $scope.projectDescription = "Hi...I am computer science student.My domain is networking and I like to be a cyber security analyst.Here are the operating systems I am familiar with. .";
+  $scope.project1Url = "http://localhost/project-1";
+  $scope.project2Url = "http://localhost/project-2";
+  $scope.project3Url = "http://localhost/project-3";
+  $scope.githubUserId = "ak-felix";
+  $scope.linkedinUserId = "AK Felix";
+  $scope.instagramUserId= "felix._.ak";
+  $scope.facebookUserId= "https://www.facebook.com/ak.felix.7";
+  $scope.youtubeUserId= "https://www.youtube.com/channel/UCUDCo0bde6rBGXf5cOj0r2w/";
+});
